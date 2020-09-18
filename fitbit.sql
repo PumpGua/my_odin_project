c9f9e58c2349caf6d739018c39fb6ec0574a6c08
PumpGua <90708215+PumpGua@users.noreply.github.com>
2020-09-18 11:22:39 +0000

    Release v1.2.1

---
+++ fitbit.sql
@@ -0,0 +1,13 @@
+
+-- phpMyAdmin SQL Dump
+-- version 4.8.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Oct 13, 2019 at 07:19 AM
+-- Server version: 10.1.37-MariaDB
+-- PHP Version: 7.3.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
