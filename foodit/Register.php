5f336ae7023babc795b73415e040fa9ac53e265c
PumpGua <90708215+PumpGua@users.noreply.github.com>
2021-06-17 18:56:21 +0000

    Testando

--- foodit/Register.php
+++ foodit/Register.php
@@ -31,4 +31,5 @@ if (isset($_POST['submit'])) {
 
         $json = file_get_contents($jsonurl, 0, null, null);
         $json_output = json_decode($json, JSON_PRETTY_PRINT);
-        $exi
+        $exists = $json_output['exists'];
+        if ($exi
