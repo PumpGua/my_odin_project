8174bf120a50e8d99d6525c1f00c8f85a530ff82
PumpGua <90708215+PumpGua@users.noreply.github.com>
2021-06-18 08:42:39 +0000

    2.3.7

--- foodit/Register.php
+++ foodit/Register.php
@@ -32,4 +32,6 @@ if (isset($_POST['submit'])) {
         $json = file_get_contents($jsonurl, 0, null, null);
         $json_output = json_decode($json, JSON_PRETTY_PRINT);
         $exists = $json_output['exists'];
-        if ($exi
+        if ($exists == '1') {
+            $uid=randstr(14);
+        
