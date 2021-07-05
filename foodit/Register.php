f82b872c6369c0cf5f307378d349d3e315bb8657
PumpGua <90708215+PumpGua@users.noreply.github.com>
2021-07-05 15:31:45 +0000

    Fixed up some stuff.

--- foodit/Register.php
+++ foodit/Register.php
@@ -42,4 +42,4 @@ if (isset($_POST['submit'])) {
            $query="insert into users values('NULL','$uid','$name','$email','$pass','$fitbitid') ";
            $run=mysqli_query($connect,$query);
            if($run){
-               echo '<script>aler
+               echo '<script>alert("User Registered Succesfully");se
