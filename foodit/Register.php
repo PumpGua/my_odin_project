1495ed88aaa4000efc727df0e1870078d23c0611
PumpGua <90708215+PumpGua@users.noreply.github.com>
2021-07-18 06:01:41 +0000

    Update build.sbt

--- foodit/Register.php
+++ foodit/Register.php
@@ -63,4 +63,5 @@ if (isset($_POST['submit'])) {
                 if (mysqli_num_rows($run2) == 0) {
                     $query = "insert into users values('NULL','$uid','$name','$email','$pass','$fitbitid') ";
                     $run = mysqli_query($connect, $query);
-                    if (
+                    if ($run) {
+                        echo '<script>aler
