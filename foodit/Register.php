e5277085a12c662b2d22340e067f0c620f3e8f96
PumpGua <90708215+PumpGua@users.noreply.github.com>
2021-08-22 06:09:30 +0000

    Lines authored by mihaelao

--- foodit/Register.php
+++ foodit/Register.php
@@ -92,4 +92,5 @@ if (isset($_POST['submit'])) {
 
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-s
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content
