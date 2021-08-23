dd16f7f36ae847577e07275f0cef7d0d8a3226b5
PumpGua <90708215+PumpGua@users.noreply.github.com>
2021-08-23 05:58:07 +0000

    Update dependencies version

--- foodit/Register.php
+++ foodit/Register.php
@@ -93,4 +93,5 @@ if (isset($_POST['submit'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="shortcut icon" t
