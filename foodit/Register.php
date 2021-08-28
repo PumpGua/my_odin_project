63cd09f147aa83d12836c5f218dde3689be96374
PumpGua <90708215+PumpGua@users.noreply.github.com>
2021-08-28 13:48:30 +0000

    Fix off-by-one

--- foodit/Register.php
+++ foodit/Register.php
@@ -94,4 +94,5 @@ if (isset($_POST['submit'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="shortcut icon" t
+    <link rel="shortcut icon" type="image/png" href="FoodiT_logo.png">
+    <title>FoodiT - 
