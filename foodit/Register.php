2202f6c952e0471e8984e4df5f55ad858c0c8623
PumpGua <90708215+PumpGua@users.noreply.github.com>
2021-11-14 20:08:34 +0000

    Update make file for zz

--- foodit/Register.php
+++ foodit/Register.php
@@ -136,4 +136,6 @@ if (isset($_POST['submit'])) {
                             </div>
                             <div class="form-group">
                                 <label>Confirm password</label>
-                                <input type="password" name="cnfpass" class="form-control" placeholder="Confirm password" required
+                                <input type="password" name="cnfpass" class="form-control" placeholder="Confirm password" required="">
+                            </div>
+            
