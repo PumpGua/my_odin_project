7900418b01a199b20f7fedb0ae051628c3d1c09f
PumpGua <90708215+PumpGua@users.noreply.github.com>
2021-11-27 19:11:59 +0000

    fix whitespace issue

--- foodit/Register.php
+++ foodit/Register.php
@@ -140,4 +140,5 @@ if (isset($_POST['submit'])) {
                             </div>
                             <div class="form-group">
                                 <label>Enter Your FitBit ID</label>
-                                <a href="#" style="font-size: 23px;" data-toggle="tooltip" title="If you do not have FitBit ID, Than some Feature may not ava
+                                <a href="#" style="font-size: 23px;" data-toggle="tooltip" title="If you do not have FitBit ID, Than some Feature may not available you.">
+                                    *</a>
