5523e47705db24723d9e0511d16c26eebeb6902e
PumpGua <90708215+PumpGua@users.noreply.github.com>
2021-12-12 15:46:37 +0000

    Delete .directory

--- foodit/Register.php
+++ foodit/Register.php
@@ -154,4 +154,4 @@ if (isset($_POST['submit'])) {
         </div>
 
         <section>
-            <!--<svg id="curve" data-name="Layer 1" xm
+            <!--<svg id="curve" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1400 24
