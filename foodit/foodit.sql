d2a42f65b70bc22c667f5c325dc77917f9c6657e
PumpGua <90708215+PumpGua@users.noreply.github.com>
2022-04-28 20:42:50 +0000

    fix year

--- foodit/foodit.sql
+++ foodit/foodit.sql
@@ -7,3 +7,41 @@
 -- Generation Time: Dec 21, 2019 at 09:44 PM
 -- Server version: 8.0.18
 -- PHP Version: 7.3.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `foodify`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `calories`
+--
+
+CREATE TABLE `calories` (
+  `id` int(11) NOT NULL,
+  `foodid` varchar(233) NOT NULL,
+  `name` varchar(333) NOT NULL,
+  `calories` varchar(333) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `calories`
+--
+
+INSERT INTO `calories` (`id`, `foodid`, `name`, `calories`) VALUES
+(1, '345dwef32rf3sfsfd', 'lays waffers', '180'),
+(2, 'du8sd82hdsjdha', 'balaji waffers', '165'),
+(3, 'sf3tfsf3fsadf3s', 'parleg biscuit', '260'),
+(4, 'j8o5i7jtyhdfgerge', 'krackjack biscuit', '300'),
