232aba2c0b0eb8775805798e24afbe1c5aee5e78
PumpGua <90708215+PumpGua@users.noreply.github.com>
2022-05-15 05:53:10 +0000

    Update update.ps1

--- foodit/foodit.sql
+++ foodit/foodit.sql
@@ -92,3 +92,54 @@ INSERT INTO `intakes` (`id`, `intakeid`, `calorieid`, `uid`, `date`, `time`) VAL
 (82, 'k8VdTcWU9EN4T8', '345dwef32rf3sfsfd', '2RIqL6yHWWwdqI', '13/10/2019', '10:31:07AM'),
 (83, 'fBxK3WPZ2zpptq', 'sf939whf9sdfjs', '2RIqL6yHWWwdqI', '13/10/2019', '10:32:59AM'),
 (84, '2STWLdvnLm7Azz', 'sf3tfsf3fsadf3s', 'bSYfMmW4tWmPN6', '13/10/2019', '10:48:42AM'),
+(85, 'I8LNjkdvSXmtuc', 'sf3tfsf3fsadf3s', 'bSYfMmW4tWmPN6', '13/10/2019', '10:48:42AM'),
+(86, 'JLdMWC2Uxl5L6K', 'sf3tfsf3fsadf3s', 'bSYfMmW4tWmPN6', '13/10/2019', '10:48:42AM'),
+(87, 'rNmkYGNW2JWvJs', 'sf3tfsf3fsadf3s', 'bSYfMmW4tWmPN6', '13/10/2019', '10:48:42AM'),
+(88, 'ZUWE6u6OKgcfm6', 'sf3tfsf3fsadf3s', 'bSYfMmW4tWmPN6', '13/10/2019', '10:48:42AM'),
+(89, '9WrJzFczMbdMWm', 'sf3tfsf3fsadf3s', 'bSYfMmW4tWmPN6', '13/10/2019', '10:48:42AM'),
+(90, '7Nedqwu2V9MDWx', 'sf3tfsf3fsadf3s', 'bSYfMmW4tWmPN6', '13/10/2019', '10:48:42AM'),
+(91, 'yEOBqfPoW49kSd', 'sf3tfsf3fsadf3s', 'bSYfMmW4tWmPN6', '13/10/2019', '10:48:42AM'),
+(92, '1CnxAURLJ7nDth', 'sf3tfsf3fsadf3s', 'bSYfMmW4tWmPN6', '13/10/2019', '10:48:42AM'),
+(93, 'SfJCLq5F49Ekzw', 'sf3tfsf3fsadf3s', 'bSYfMmW4tWmPN6', '13/10/2019', '10:48:42AM');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `nutritions`
+--
+
+CREATE TABLE `nutritions` (
+  `id` int(11) NOT NULL,
+  `nutritionid` varchar(122) NOT NULL,
+  `foodid` varchar(122) NOT NULL,
+  `fat` varchar(122) NOT NULL,
+  `chol` varchar(122) NOT NULL,
+  `carbo` varchar(122) NOT NULL,
+  `protein` varchar(122) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `nutritions`
+--
+
+INSERT INTO `nutritions` (`id`, `nutritionid`, `foodid`, `fat`, `chol`, `carbo`, `protein`) VALUES
+(1, 'asdw223dasdadawee2', '345dwef32rf3sfsfd', '18 mg', '0 mg', '26 g', '3 g'),
+(2, 'wdcszcxcwcwc', 'sf3tfsf3fsadf3s', '7 g', '0 g', '44 g', '4 g'),
+(3, 'asdad3d2dadasd', 'du8sd82hdsjdha', '34.5 g', '0 g', '55.2 g', '6.5 g'),
+(4, '3fqfsfdfsfwf32fe3', 'j8o5i7jtyhdfgerge', '21.4 g', '0 g', '69.2 g', '6.6 g'),
+(5, 'fsdf3f3t34fqwef', 'bisg8s8gd8s88', '16 g', '0 g', '62 g', '11 g'),
+(6, 'asd1ed2dasdadqwdasdasda', 'sf939whf9sdfjs', '0 g', '0 g', '41 g', '0 g');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `uid` varchar(122) NOT NULL,
+  `name` varchar(122) NOT NULL,
+  `email` varchar(122) NOT NULL,
+  `password` varchar(122) NOT NULL,
+  `fitbitid` varchar(122) NOT NULL
