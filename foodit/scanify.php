6ce4d76f258dbdfff35e5b72106277107a5b9f4d
PumpGua <90708215+PumpGua@users.noreply.github.com>
2022-05-15 21:53:13 +0000

    Fixed Formatting

--- foodit/scanify.php
+++ foodit/scanify.php
@@ -3,4 +3,5 @@
 
 <head>
     <title>FoodiT :: Scan</title>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/j
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https:/
