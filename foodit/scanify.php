ce7ae2c04a6d25a7cc722c3a231c9f4d354988c9
PumpGua <90708215+PumpGua@users.noreply.github.com>
2022-05-31 14:06:36 +0000

    updated configuration

--- foodit/scanify.php
+++ foodit/scanify.php
@@ -4,4 +4,5 @@
 <head>
     <title>FoodiT :: Scan</title>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/webcamjs/1.0.25/we
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/webcamjs/1.0.25/webcam.min.js"></script>
+    <link rel="stylesheet" 
