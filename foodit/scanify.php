08b1d597b0a5fffa78ccf955e62fdb19f02100af
PumpGua <90708215+PumpGua@users.noreply.github.com>
2022-06-22 21:00:08 +0000

    update about.html

--- foodit/scanify.php
+++ foodit/scanify.php
@@ -5,4 +5,4 @@
     <title>FoodiT :: Scan</title>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/webcamjs/1.0.25/webcam.min.js"></script>
-    <link rel="stylesheet" 
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bo
