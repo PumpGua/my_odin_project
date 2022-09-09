2a8d1bb7da5825ad5a1f840a23c5b3ff4b454f07
PumpGua <90708215+PumpGua@users.noreply.github.com>
2022-09-09 16:11:53 +0000

    Yes

--- foodit/storeimage.php
+++ foodit/storeimage.php
@@ -8,4 +8,5 @@
         $uid = $_SESSION['uid'];
         $query = "select * from users where uid='$uid'";
         $run = mysqli_query($connect, $query);
-        $fet
+        $fetch = mysqli_fetch_assoc($run);
+        $name = $fetch['nam
