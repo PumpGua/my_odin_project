6cd33abc0c48faf310c139136a2ff9dacc944d31
PumpGua <90708215+PumpGua@users.noreply.github.com>
2022-10-22 21:56:30 +0000

    added more notes

--- foodit/storeimage.php
+++ foodit/storeimage.php
@@ -86,4 +86,4 @@
      <meta name="viewport" content="width=device-width, initial-scale=1.0">
      <meta http-equiv="X-UA-Compatible" content="ie=edge">
      <title>FoodiT :: Summary</title>
-     <script src="https://cdnjs.cloud
+     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></
