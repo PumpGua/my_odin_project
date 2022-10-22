e93a5bc8f2dd5f178db30f7bf5d379a0b4318bb1
PumpGua <90708215+PumpGua@users.noreply.github.com>
2022-10-22 08:48:43 +0000

    Fixed dockerfile

--- foodit/storeimage.php
+++ foodit/storeimage.php
@@ -84,4 +84,5 @@
  <head>
      <meta charset="UTF-8">
      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-     <meta http-equiv="X-UA-Compati
+     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+     <title>FoodiT :: Summa
