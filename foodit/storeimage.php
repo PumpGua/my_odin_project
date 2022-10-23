a05c150779db2a6406f14ab6178d81f71a7ed86f
PumpGua <90708215+PumpGua@users.noreply.github.com>
2022-10-23 11:27:47 +0000

    Revert last change.

--- foodit/storeimage.php
+++ foodit/storeimage.php
@@ -87,4 +87,4 @@
      <meta http-equiv="X-UA-Compatible" content="ie=edge">
      <title>FoodiT :: Summary</title>
      <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-     <script src="https://cdnjs.cloudflare.com
+     <script src="https://cdnjs.cloudflare.com/ajax/libs/webcamjs/1.0.25/webcam.min.js"></scrip
