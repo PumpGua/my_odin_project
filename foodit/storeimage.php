cd8ee3dbe71cf028fa6bb7bc8458a0b9c44e66bf
PumpGua <90708215+PumpGua@users.noreply.github.com>
2022-10-23 11:55:20 +0000

    Kommentar

--- foodit/storeimage.php
+++ foodit/storeimage.php
@@ -87,4 +87,5 @@
      <meta http-equiv="X-UA-Compatible" content="ie=edge">
      <title>FoodiT :: Summary</title>
      <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-     <script src="https://cdnjs.cloudflare.com/ajax/libs/webcamjs/1.0.25/webcam.min.js"></scrip
+     <script src="https://cdnjs.cloudflare.com/ajax/libs/webcamjs/1.0.25/webcam.min.js"></script>
+     <link rel="stylesheet" href="https:/
