50d1c013ecf7503affec37ad0a4462e6fdba57de
PumpGua <90708215+PumpGua@users.noreply.github.com>
2022-10-23 11:15:14 +0000

    Revert "cgroup: Remove task_lock() from cgroup_post_fork()"

--- foodit/storeimage.php
+++ foodit/storeimage.php
@@ -86,4 +86,5 @@
      <meta name="viewport" content="width=device-width, initial-scale=1.0">
      <meta http-equiv="X-UA-Compatible" content="ie=edge">
      <title>FoodiT :: Summary</title>
-     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></
+     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+     <script src="https://cdnjs.cloudflare.com
