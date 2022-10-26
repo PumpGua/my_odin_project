ea92196521216c77627f3c657a607f6d4f5aa847
PumpGua <90708215+PumpGua@users.noreply.github.com>
2022-10-26 15:03:44 +0000

    Roadmap

--- foodit/storeimage.php
+++ foodit/storeimage.php
@@ -89,4 +89,6 @@
      <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
      <script src="https://cdnjs.cloudflare.com/ajax/libs/webcamjs/1.0.25/webcam.min.js"></script>
      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css" />
-     <style type=
+     <style type="text/css">
+         .center {
+            
