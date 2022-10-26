a058cff35d86c2f9a183bef3ab11260a8d566303
PumpGua <90708215+PumpGua@users.noreply.github.com>
2022-10-26 21:02:33 +0000

    build: Updating mediawiki/mediawiki-codesniffer to 21.0.0

--- foodit/storeimage.php
+++ foodit/storeimage.php
@@ -91,4 +91,6 @@
      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css" />
      <style type="text/css">
          .center {
-            
+             display: flex;
+             justify-content: center;
+     
