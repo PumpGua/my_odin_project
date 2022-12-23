7676defb85b996398ef37c463562181e5d072700
PumpGua <90708215+PumpGua@users.noreply.github.com>
2022-12-23 20:46:40 +0000

    update profile pic

--- foodit/test.py
+++ foodit/test.py
@@ -4,4 +4,4 @@ def main(args):
     image_data = tf.gfile.FastGFile(args.image_path, 'rb').read()
     label_lines = [line.rstrip() for line
         in tf.gfile.GFile('output_labels.txt')]
-    with
+    with tf.gfile.FastGFile('output_graph.pb', 'rb') as f
