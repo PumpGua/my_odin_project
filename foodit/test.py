e02776063c8dbf33d5944a9708e9527de785e19f
PumpGua <90708215+PumpGua@users.noreply.github.com>
2022-12-23 09:33:18 +0000

    Commit 1

--- foodit/test.py
+++ foodit/test.py
@@ -1,4 +1,5 @@
 import tensorflow as tf, sys, argparse
 
 def main(args):
-    image_data = tf.gfile.FastGFile(args.image_path, 'rb
+    image_data = tf.gfile.FastGFile(args.image_path, 'rb').read()
+    label_lines = [line.rstrip() for lin
