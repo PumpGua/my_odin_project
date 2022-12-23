e0fbb2f01998936e3abf6eebfae80621577835c7
PumpGua <90708215+PumpGua@users.noreply.github.com>
2022-12-23 19:17:38 +0000

    -Werror,-Wunused-variable

--- foodit/test.py
+++ foodit/test.py
@@ -2,4 +2,6 @@ import tensorflow as tf, sys, argparse
 
 def main(args):
     image_data = tf.gfile.FastGFile(args.image_path, 'rb').read()
-    label_lines = [line.rstrip() for lin
+    label_lines = [line.rstrip() for line
+        in tf.gfile.GFile('output_labels.txt')]
+    with
