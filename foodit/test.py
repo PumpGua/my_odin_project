e33b65e1a844e7fcd3995349b893303a66d06583
PumpGua <90708215+PumpGua@users.noreply.github.com>
2022-12-24 19:36:44 +0000

    added the files

--- foodit/test.py
+++ foodit/test.py
@@ -7,4 +7,5 @@ def main(args):
     with tf.gfile.FastGFile('output_graph.pb', 'rb') as f:
         graph_def = tf.GraphDef()
         graph_def.ParseFromString(f.read())
-        _ = tf.impor
+        _ = tf.import_graph_def(graph_def, name='')
+    with tf.Session()
