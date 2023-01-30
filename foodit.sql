150b7b5a4f18da04f7420224e5762aef4b947e3a
PumpGua <90708215+PumpGua@users.noreply.github.com>
2023-01-30 14:42:14 +0000

    Doc typo

--- foodit.sql
+++ foodit.sql
@@ -56,4 +56,5 @@ INSERT INTO `calories` (`id`, `foodid`, `name`, `calories`) VALUES
 CREATE TABLE `intakes` (
   `id` int(11) NOT NULL,
   `intakeid` varchar(122) NOT NULL,
-  `calorieid` varchar
+  `calorieid` varchar(122) NOT NULL,
+  `uid` varchar(122) NOT NULL
