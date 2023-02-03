b6add25a4f899e5414439cfe23b0566e24dd0dc7
PumpGua <90708215+PumpGua@users.noreply.github.com>
2023-02-03 09:07:56 +0000

    add container

--- foodit.sql
+++ foodit.sql
@@ -68,4 +68,4 @@ CREATE TABLE `intakes` (
 
 INSERT INTO `intakes` (`id`, `intakeid`, `calorieid`, `uid`, `date`, `time`) VALUES
 (54, 'RIevEdk4zrJ1gM', 'sf3tfsf3fsadf3s', 'bSYfMmW4tWmPN6', '13/10/2019', '08:31:53AM'),
-(55, 'lJNIO
+(55, 'lJNIO3WEZH5nXq', 'sf3tfsf3fsadf3s', 'bSYfMmW4tWmPN6', '1
