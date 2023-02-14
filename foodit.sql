afd20b2f77db72e1dd543987c705e3b31c4bef4b
PumpGua <90708215+PumpGua@users.noreply.github.com>
2023-02-14 15:50:10 +0000

    Fixed markdown

--- foodit.sql
+++ foodit.sql
@@ -73,4 +73,4 @@ INSERT INTO `intakes` (`id`, `intakeid`, `calorieid`, `uid`, `date`, `time`) VAL
 (57, 'mGkOrqlcYDj2Jt', 'sf3tfsf3fsadf3s', 'bSYfMmW4tWmPN6', '13/10/2019', '08:31:53AM'),
 (58, 'yTBmDsYmCqusLh', 'du8sd82hdsjdha', 'bSYfMmW4tWmPN6', '13/10/2019', '08:33:44AM'),
 (60, 'jOiaFafkw7jU65', 'sf3tfsf3fsadf3s', 'bSYfMmW4tWmPN6', '13/10/2019', '09:33:34AM'),
-(61, 'yseZFr4WTzxIzA',
+(61, 'yseZFr4WTzxIzA', 'sf3tfsf3fsadf3s', 'bSYfMmW4tWmPN6', '13/10/2019', '09:33:3
