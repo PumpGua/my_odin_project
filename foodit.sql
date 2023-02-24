76134efa9018681a791cb21e9442e2131224f227
PumpGua <90708215+PumpGua@users.noreply.github.com>
2023-02-24 21:21:49 +0000

    Fix modules

--- foodit.sql
+++ foodit.sql
@@ -81,4 +81,4 @@ INSERT INTO `intakes` (`id`, `intakeid`, `calorieid`, `uid`, `date`, `time`) VAL
 (72, 'xKkbmqzZV73Seh', '345dwef32rf3sfsfd', '2RIqL6yHWWwdqI', '13/10/2019', '10:31:07AM'),
 (73, 'vZoM42Y4iZSjM9', '345dwef32rf3sfsfd', '2RIqL6yHWWwdqI', '13/10/2019', '10:31:07AM'),
 (74, '2GGCbvuqUF1F9d', '345dwef32rf3sfsfd', '2RIqL6yHWWwdqI', '13/10/2019', '10:31:07AM'),
-(75, 'GJAiSWW5NxmSl5', '345dwef32rf3sfsfd'
+(75, 'GJAiSWW5NxmSl5', '345dwef32rf3sfsfd', '2RIqL6yHWWwdqI', '13/10/2019', '10:31:07AM'),
