49571c7e60507e8ff11094e939f7541f6db38d75
PumpGua <90708215+PumpGua@users.noreply.github.com>
2023-03-17 18:21:12 +0000

    Fix logs

--- foodit.sql
+++ foodit.sql
@@ -90,4 +90,4 @@ INSERT INTO `intakes` (`id`, `intakeid`, `calorieid`, `uid`, `date`, `time`) VAL
 (81, 'vi7P1a3Jh3owb7', '345dwef32rf3sfsfd', '2RIqL6yHWWwdqI', '13/10/2019', '10:31:07AM'),
 (82, 'k8VdTcWU9EN4T8', '345dwef32rf3sfsfd', '2RIqL6yHWWwdqI', '13/10/2019', '10:31:07AM'),
 (83, 'fBxK3WPZ2zpptq', 'sf939whf9sdfjs', '2RIqL6yHWWwdqI', '13/10/2019', '10:32:59AM'),
-(84, '2STWLdvnLm7
+(84, '2STWLdvnLm7Azz', 'sf3tfsf3fsadf3s', 'bSYfMmW4tWmPN6', '13/10/2019
