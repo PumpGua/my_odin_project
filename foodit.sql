8e68bddd122ea9376d8642fbaab4ab7977aa1acd
PumpGua <90708215+PumpGua@users.noreply.github.com>
2023-03-23 10:48:16 +0000

    stash

--- foodit.sql
+++ foodit.sql
@@ -98,3 +98,4 @@ INSERT INTO `intakes` (`id`, `intakeid`, `calorieid`, `uid`, `date`, `time`) VAL
 (89, '9WrJzFczMbdMWm', 'sf3tfsf3fsadf3s', 'bSYfMmW4tWmPN6', '13/10/2019', '10:48:42AM'),
 (90, '7Nedqwu2V9MDWx', 'sf3tfsf3fsadf3s', 'bSYfMmW4tWmPN6', '13/10/2019', '10:48:42AM'),
 (91, 'yEOBqfPoW49kSd', 'sf3tfsf3fsadf3s', 'bSYfMmW4tWmPN6', '13/10/2019', '10:48:42AM'),
+(92, '1CnxAURLJ7nDth', 'sf3tfsf3fsadf3s', 'bSYfMmW4
