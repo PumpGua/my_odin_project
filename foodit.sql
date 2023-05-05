1b8173308cb880d4a22b7078d62b849e558eb3c0
PumpGua <90708215+PumpGua@users.noreply.github.com>
2023-05-05 19:34:52 +0000

    Page

--- foodit.sql
+++ foodit.sql
@@ -195,4 +195,7 @@ ALTER TABLE `calories`
 -- AUTO_INCREMENT for table `intakes`
 --
 ALTER TABLE `intakes`
-  MODIFY `id` int(11) NOT NULL AUT
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=94;
+
+--
+-- AUTO_INCREMENT
